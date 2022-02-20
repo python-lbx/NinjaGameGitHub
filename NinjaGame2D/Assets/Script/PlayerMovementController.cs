193cc9a3a0bcab7d242bd27c25eb92a2f61fe419
Fong <lbx1924@gmail.com>
2022-02-20 21:39:29 +0800

    下蹲代碼

--- NinjaGame2D/Assets/Script/PlayerMovementController.cs
+++ NinjaGame2D/Assets/Script/PlayerMovementController.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     Rigidbody2D rb;
-    Collider2D coll;
+    CapsuleCollider2D capsulecoll;
     Animator anim;
 
     [Header("移動參數")]
     public float speed;
     private float horizontalmove;
+
+    [Header("角色狀態")]
+    public bool isCrouch;
     
+    Vector2 colliderStandSize; //站立碰撞框尺寸
+    Vector2 colliderStandOffset; //站立碰撞框位置
+    Vector2 colliderCrouchSize; //下蹲碰撞框尺寸
+    Vector2 colliderCrouchOffset; //下蹲碰撞框位置
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        coll = GetComponent<Collider2D>();
+        capsulecoll = GetComponent<CapsuleCollider2D>();
         anim = GetComponent<Animator>();
+
+        //獲取碰撞框參數
+        colliderStandSize = capsulecoll.size; //站立
+        colliderStandOffset = capsulecoll.offset;
+        colliderCrouchSize = new Vector2(0.7667918f,0.7667921f); //下蹲
+        colliderCrouchOffset = new Vector2(-0.07691693f,-0.5154546f);
+
     }
 
     // Update is called once per frame
@@ -35,6 +50,15 @@ public class PlayerMovementController : MonoBehaviour
         rb.velocity = new Vector2(horizontalmove * speed,rb.velocity.y);
         
         FilpDirection();
+
+        if(Input.GetButton("Crouch"))
+        {
+            Crouch();
+        }
+        else if(!Input.GetButton("Crouch") && isCrouch)
+        {
+            StandUp();
+        }
     }
 
     void FilpDirection()
@@ -49,4 +73,17 @@ public class PlayerMovementController : MonoBehaviour
         }
     }
 
+    void Crouch()
+    {
+        isCrouch = true;
+        capsulecoll.size = colliderCrouchSize;
+        capsulecoll.offset = colliderCrouchOffset;
+    }
+
+    void StandUp()
+    {
+       isCrouch = false;
+       capsulecoll.size = colliderStandSize;
+       capsulecoll.offset = colliderStandOffset;
+    }
 }
