5afbd94dfbe58052b2f444ac8913226af62ad39b
Fong <lbx1924@gmail.com>
2022-03-01 10:06:22 +0800

    場景切換
    小怪與頭目關卡記錄

--- NinjaGame2D/Assets/Script/Scene/LevelManager.cs
+++ NinjaGame2D/Assets/Script/Scene/LevelManager.cs
@@ -7,12 +7,16 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     public int levelsUnlocked;
+    public int boss_levelsUnlocked;
     public Button[] buttons;
+    public Button[] boss_buttons;
 
     // Start is called before the first frame update
     void Start()
     {
+        //普通關卡
         levelsUnlocked = PlayerPrefs.GetInt("levelsUnlocked",1);
+        boss_levelsUnlocked = PlayerPrefs.GetInt("boss_levelsUnlocked");
 
         for(int i=0; i<buttons.Length;i++)
         {
@@ -22,6 +26,18 @@ public class LevelManager : MonoBehaviour
         for(int i=0; i<levelsUnlocked;i++)
         {
             buttons[i].interactable = true;
+        }
+
+
+        //首領戰
+        for(int i=0;i<boss_buttons.Length;i++)
+        {
+            boss_buttons[i].interactable = false;
+        }
+
+        for(int i=0;i<boss_levelsUnlocked;i++)
+        {
+            boss_buttons[i].interactable = true;
         }
     }
 
--- NinjaGame2D/Assets/Script/Scene/TranslateDoor.cs
+++ NinjaGame2D/Assets/Script/Scene/TranslateDoor.cs
@@ -8,12 +8,17 @@ public class TranslateDoor : MonoBehaviour
     Collider2D coll;
     public bool NextLevelIsBoss;
     public bool NextLevelIsEnemy;
+    public int currentLevel;
+    public int nextLevel;
+    public int currentBossLevel;
+    public int nextBossLevel;
     public string BossName;
     public string LevelName;
     // Start is called before the first frame update
     void Start()
     {
         coll = GetComponent<Collider2D>();
+        currentLevel = PlayerPrefs.GetInt("levelsUnlocked");
     }
 
     // Update is called once per frame
@@ -28,11 +33,23 @@ public class TranslateDoor : MonoBehaviour
         {
             if(NextLevelIsBoss)
             {
+                if(nextBossLevel > currentBossLevel)
+                {
+                    PlayerPrefs.SetInt("boss_levelsUnlocked",nextBossLevel);
+                }
+                Debug.Log("BossLevel"+PlayerPrefs.GetInt("boss_levelsUnlocked")+"Unlocked");
                 SceneManager.LoadScene(BossName);
             }
             
             if(NextLevelIsEnemy)
             {
+
+                if(nextLevel > currentLevel )
+                {
+                    PlayerPrefs.SetInt("levelsUnlocked",nextLevel);
+                }
+            
+                Debug.Log("Level"+PlayerPrefs.GetInt("levelsUnlocked")+"Unlocked");
                 SceneManager.LoadScene(LevelName);
             }
         }
