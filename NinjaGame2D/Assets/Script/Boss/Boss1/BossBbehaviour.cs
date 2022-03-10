c854c2e4be8248d4da94a9312afca32df05e524a
Fong <lbx1924@gmail.com>
2022-03-10 10:38:26 +0800

    Update BossBbehaviour.cs

--- NinjaGame2D/Assets/Script/Boss/Boss1/BossBbehaviour.cs
+++ NinjaGame2D/Assets/Script/Boss/Boss1/BossBbehaviour.cs
@@ -4,23 +4,99 @@ using UnityEngine;
 
 public class BossBbehaviour : MonoBehaviour
 {
+    [Header("巡邏")]
+    public float Patrolspeed;
+    public float startWaitTime;
+    public float WaitTime;
+
+    public Transform movePos;
+    public Transform leftDownPos;
+    public Transform rightUpPos;
+
+    [Header("橫衝")]
     public Transform[] point;
     public int BossBRndPos;
     public Transform MovePos;
-    public int speed;
+    public int Rushspeed;
+    public int time;
+
+    [Header("階段")]
+    public bool phase;
+    public float phaseTime;
+    public float StartphaseTime;
+    public enum Status {patrol,HRush};
+    public Status BossB_Status;
+
     // Start is called before the first frame update
     void Start()
     {
         BossBRndPos = Random.Range(0,4);
-        transform.position = point[BossBRndPos].transform.position;
+        phaseTime = 5;
+        BossB_Status = Status.patrol;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch(BossB_Status)
+        {
+            case Status.patrol:
+            if(phaseTime>0)
+            {
+                RndPatrol();
+                phaseTime -= Time.deltaTime;
+            }
+            else if(phaseTime<=0)
+            {
+                transform.position = point[BossBRndPos].transform.position;
+                BossB_Status = Status.HRush;
+            }
+            break;
+            case Status.HRush:
+            if(time == 4)
+            {
+                time = 0;
+                phaseTime =5;
+                BossB_Status = Status.patrol;
+            }
+            else
+            {
+                HorizontalRush();
+            }
+            break;
+            
+        }
+ 
+    }
 
-        transform.position = Vector2.MoveTowards(transform.position,MovePos.position,speed * Time.deltaTime);
+    void RndPatrol()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,movePos.position,Patrolspeed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position,movePos.position) < 0.1f)
+        {
+            if(WaitTime <= 0)
+            {
+                movePos.position =  GetRandomPos();
+                WaitTime = startWaitTime;
+            }
+            else
+            {
+                WaitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    Vector2 GetRandomPos()
+    {
+        Vector2 rndPos = new Vector2(Random.Range(leftDownPos.position.x,rightUpPos.position.x),Random.Range(leftDownPos.position.y,rightUpPos.position.y));
+        return rndPos;
+    }
+
+    void HorizontalRush()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,MovePos.position,Rushspeed * Time.deltaTime);
 
         if(BossBRndPos == 0)
         {
@@ -42,6 +118,7 @@ public class BossBbehaviour : MonoBehaviour
 
         if(Vector2.Distance(this.transform.position,MovePos.position)<0.1f)
         {
+            time++;
             BossBRndPos = Random.Range(0,4);
             transform.position = point[BossBRndPos].transform.position;
         }
